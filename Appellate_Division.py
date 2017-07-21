477fe2dae39b75379f010f9596035bc4b900c800
zetafi <jerryzcp@gmail.com>
2017-07-20 20:03:01 -0500

    get appellate decision result

--- Appellate_Division.py
+++ Appellate_Division.py
@@ -49,48 +49,79 @@ def exploring_ap():
         header = apdata.next()
 
         count = 0
-        nonopn = []
+        result = []
 
+        keyword = "The judgment or decision is: "
 
-        countt = 0
 
         for row in apdata:
-            # tdate = datetime.datetime.strptime(row[25], "%m/%d/%Y").date().strftime("%B %d, %Y")
-            #
-            # fdates = re.split("[\'\"],\su[\'\"]", row[27])
-            # fdates[0] = fdates[0].replace("[u\'", "")
-            # fdates[-1] = fdates[-1].replace("\']", "")
-            #
+            tdate = datetime.datetime.strptime(row[25], "%m/%d/%Y").date().strftime("%B %-d, %Y")
+
+            fdates = re.split("[\'\"],\su[\'\"]", row[27])
+            fdates[0] = fdates[0].replace("[u\'", "")
+            fdates[-1] = fdates[-1].replace("\']", "")
+
             filings = re.split("[\'\"],\su[\'\"]", row[28])
             filings[0] = filings[0].replace("[u\'", "")
             filings[-1] = filings[-1].replace("\']", "")
 
-            if "OPINION and JUDGMENT filed" not in row[28]:
-                cond = ("terminated" in row[28]) or ("dismissed" in row[28]) \
-                       or ("terminate" in row[28]) or ("dismiss" in row[28]) \
-                       or ("Miscellaneous" in row[28]) or ("terminating" in row[28])
-                nonopn.append(row[26] + "@" + str(len(filings)) + "@" + str(cond))
+            if "OPINION and JUDGMENT filed" in row[28]:
+                for ind in range(len(filings)):
+                    filings[ind] = filings[ind].replace("The j decision is:", "The judgment or decision is:")
 
-        nonopn = set(nonopn)
+                    cond = ("OPINION and JUDGMENT filed" in filings[ind]) \
+                           and ("The judgment or decision is:" in filings[ind]) \
+                           and not ("This entry was made in error" in filings[ind])
 
-        res = []
-        for url in nonopn:
-            res.append(url.split("@"))
-        ress = sorted(res, key=lambda url:int(url[1]))
+                    #cond2 =
+                    if cond:
+                        count += 1
+                        judgement = filings[ind]
+                        findindex = judgement.find(keyword)
+                        resbegin = findindex + len(keyword)
+                        resend = judgement.find(".", resbegin, resbegin + 80)
+                        judgresult = judgement[int(resbegin):int(resend)]
 
-        for url in ress:
-            if url[2] == "False":
-                print url
+
+                        print row[26]
+                        print judgresult
+                        print judgement
+                        #print ""
 
 
-            # evtfilings = ""
-            # for ind in range(len(filings)):
-            #     if fdates[ind] == tdate:
-            #         evtfilings += filings[ind] + "\n"
-            # print row[26]
-            # print tdate
-            # print evtfilings
-            # print " "
+            # else:
+            #     evtfilings = ""
+            #     for ind in range(len(filings)):
+            #         if fdates[ind] == tdate:
+            #             evtfilings += filings[ind] + " | "
+            #
+            #     cond = ("ORDER" in evtfilings) and (
+            #     ("affirm" in evtfilings) or ("AFFIRM" in evtfilings) or ("Affirm" in evtfilings)
+            #     or ("vacat" in evtfilings) or ("VACAT" in evtfilings) or ("Vacat" in evtfilings)
+            #     or ("reverse" in evtfilings) or ("Reverse" in evtfilings) or (
+            #         "REVERSE" in evtfilings)
+            #     or ("remand" in evtfilings) or ("Remand" in evtfilings) or ("REMAND" in evtfilings))
+            #
+            #     if cond:
+            #         add = True
+
+
+
+
+
+
+        #
+        # res = []
+        # for url in result:
+        #     res.append(url.split("@"))
+        # ress = sorted(res, key=lambda url:int(url[1]))
+        #
+        # for url in ress:
+        #     if url[2] == "True":
+        #         print url
+        print count
+
+
 
 
 exploring_ap()
